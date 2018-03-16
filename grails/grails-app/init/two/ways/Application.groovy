50982f3971a958369f5e6ce0651423b9a7a4d57d
Jeff Beck <beckje01@gmail.com>
2018-03-16 09:16:03 -0500

    upgraded slueth

--- grails/grails-app/init/two/ways/Application.groovy
+++ grails/grails-app/init/two/ways/Application.groovy
@@ -3,10 +3,10 @@ package two.ways
 import grails.boot.GrailsApp
 import grails.boot.config.GrailsAutoConfiguration
 import groovy.util.logging.Slf4j
-import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty
-import org.springframework.cloud.sleuth.zipkin.ZipkinSpanReporter
-import org.springframework.context.annotation.Bean
-import zipkin.Span
+//import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty
+//import org.springframework.cloud.sleuth.zipkin.ZipkinSpanReporter
+//import org.springframework.context.annotation.Bean
+//import zipkin.Span
 
 @Slf4j
 class Application extends GrailsAutoConfiguration {
@@ -14,14 +14,14 @@ class Application extends GrailsAutoConfiguration {
 		GrailsApp.run(Application, args)
 	}
 
-	@Bean
-	@ConditionalOnProperty(value = "sample.zipkin.enabled", havingValue = "false")
-	public ZipkinSpanReporter spanCollector() {
-		return new ZipkinSpanReporter() {
-			@Override
-			public void report(Span span) {
-				log.info(String.format("Consumer reporting span [%s]", span));
-			}
-		};
-	}
+//	@Bean
+//	@ConditionalOnProperty(value = "sample.zipkin.enabled", havingValue = "false")
+//	public ZipkinSpanReporter spanCollector() {
+//		return new ZipkinSpanReporter() {
+//			@Override
+//			public void report(Span span) {
+//				log.info(String.format("Consumer reporting span [%s]", span));
+//			}
+//		};
+//	}
 }
