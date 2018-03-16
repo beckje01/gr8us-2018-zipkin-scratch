7477135ceff28b264b84e8b5b90f46ec18ea6a14
Jeff Beck <beckje01@gmail.com>
2018-03-16 09:11:33 -0500

    Working Zipkin

--- grails/grails-app/init/two/ways/Application.groovy
+++ grails/grails-app/init/two/ways/Application.groovy
@@ -2,9 +2,26 @@ package two.ways
 
 import grails.boot.GrailsApp
 import grails.boot.config.GrailsAutoConfiguration
+import groovy.util.logging.Slf4j
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty
+import org.springframework.cloud.sleuth.zipkin.ZipkinSpanReporter
+import org.springframework.context.annotation.Bean
+import zipkin.Span
 
+@Slf4j
 class Application extends GrailsAutoConfiguration {
-    static void main(String[] args) {
-        GrailsApp.run(Application, args)
-    }
+	public static void main(String[] args) {
+		GrailsApp.run(Application, args)
+	}
+
+	@Bean
+	@ConditionalOnProperty(value = "sample.zipkin.enabled", havingValue = "false")
+	public ZipkinSpanReporter spanCollector() {
+		return new ZipkinSpanReporter() {
+			@Override
+			public void report(Span span) {
+				log.info(String.format("Consumer reporting span [%s]", span));
+			}
+		};
+	}
 }
